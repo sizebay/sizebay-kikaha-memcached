89536941cfa5ad9731afe36ab3823437fec6d460
Sizebay <sizelabs@sizebay.com>
2016-07-29 14:12:49 -0300

    Added log detailing the endpoint and port that will be used to connect to Memcached

--- source/sizebay/kikaha/memcached/MemcachedClientProducer.java
+++ source/sizebay/kikaha/memcached/MemcachedClientProducer.java
@@ -6,11 +6,13 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Produces;
 import javax.inject.*;
 import kikaha.config.Config;
+import lombok.extern.slf4j.Slf4j;
 import net.spy.memcached.MemcachedClient;
 
 /**
  *
  */
+@Slf4j
 @Singleton
 public class MemcachedClientProducer {
 
@@ -21,10 +23,11 @@ public class MemcachedClientProducer {
 
 	@PostConstruct
 	public void loadCredentials() throws IOException {
-		final InetSocketAddress address = new InetSocketAddress(
-				config.getString("server.aws.memcached.endpoint"),
-				config.getInteger("server.aws.memcached.port")
-		);
+		final String host = config.getString("server.aws.memcached.endpoint", "127.0.0.1");
+		final int port = config.getInteger("server.aws.memcached.port");
+		log.info( "Starting connection to Memcached: " + host + ":" + port );
+
+		final InetSocketAddress address = new InetSocketAddress( host, port );
 		client = new MemcachedClient( address );
 	}
 
